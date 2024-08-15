18839bfee90667de3249fa2dae91287258301d94
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-15 20:18:43 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,15 @@
+def myfun():
+  print("Hello there")
+  print("Hey wass up sajin?")
+  
+myfun()
+myfun()
+myfun()
+'''
+Hello there
+Hey wass up sajin?
+Hello there
+Hey wass up sajin?
+Hello there
+Hey wass up sajin?
+'''
